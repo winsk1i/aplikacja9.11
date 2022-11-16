7ce1598be791000a1f64e021915f6d788d8a5474
kstajbnz <k.stajkowski@o2.pl>
2022-11-16 12:36:14 +0100

    g

--- app/src/main/java/com/barstool/aplikacja/MainActivity.kt
+++ app/src/main/java/com/barstool/aplikacja/MainActivity.kt
@@ -3,22 +3,50 @@ package com.barstool.aplikacja
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var swipe=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button2).setOnClickListener {
-            if(swipe == true) {
-                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot2)
 
-        }
-        else {
-                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot1)
-                swipe = true
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            swipe = if (swipe) {
+                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot2)
+                false
+            } else {
+                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot3)
+                true
             }
         }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            swipe = if (swipe) {
+                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot3)
+                false
+            } else {
+                findViewById<ImageView>(R.id.kot1).setImageResource(R.drawable.kot2)
+                true
+            }
+        }
+        val silniaRun=findViewById<Button>(R.id.button3)
+        val silniaInput=findViewById<EditText>(R.id.wpisz)
+
+        silniaRun.setOnClickListener {
+            val inputInt = silniaInput.text.toString().toInt()
+            var liczbaZSilni=1
+            if(inputInt==0 || inputInt==1){
+                liczbaZSilni=1
+            }
+            else{
+                for(i in 2 .. inputInt)
+                    liczbaZSilni*=i
+            }
+            val text = liczbaZSilni.toString()
+            findViewById<TextView>(R.id.wynik).text=(text)
+        }
+
     }
 }
